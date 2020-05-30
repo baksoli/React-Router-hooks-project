ffd2399b90c10e93ac770d0a9ea1347b6b7cd98e
baksoli <solyi0712@gmail.com>
2020-05-30 12:47:13 +0900

    2

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function RecipeDetail(props){
+    const {match}=props;
     return (
-        <div><h1>레시피 상세보기 : {props.match.params.no}</h1></div>
+        <div><h1>레시피 상세보기 : {match.params.no}</h1></div>
     )
 }
